1c81a68a0d17e974c256a8df8ea552cb42460e99
ashutosh <ashutoshmohapatra0907@gmail.com>
2024-02-11 20:34:09 +0530

    first-commit

---
+++ src/Pages/Claim/Claim.js
@@ -0,0 +1,88 @@
+import {View, Text, Platform} from 'react-native';
+import React from 'react';
+import Geolocation from 'react-native-geolocation-service';
+import {useEffect} from 'react';
+import {check, PERMISSIONS, RESULTS} from 'react-native-permissions';
+
+const Claim = () => {
+  useEffect(() => {
+    getLocationPermission();
+  }, []);
+
+  const getLocationPermission = () => {
+    console.log('first');
+    if (Platform.OS == 'android') {
+      check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION)
+        .then(result => {
+          switch (result) {
+            case RESULTS.UNAVAILABLE:
+              console.log(
+                'This feature is not available (on this device / in this context)',
+              );
+              break;
+            case RESULTS.DENIED:
+              console.log(
+                'The permission has not been requested / is denied but requestable',
+              );
+              break;
+            case RESULTS.LIMITED:
+              console.log(
+                'The permission is limited: some actions are possible',
+              );
+              break;
+            case RESULTS.GRANTED:
+              console.log('The permission is granted');
+              break;
+            case RESULTS.BLOCKED:
+              console.log(
+                'The permission is denied and not requestable anymore',
+              );
+              break;
+          }
+        })
+        .catch(error => {
+          console.log('ERROR: ', error);
+        });
+    } else {
+      check(PERMISSIONS.IOS.LOCATION_ALWAYS)
+        .then(result => {
+          switch (result) {
+            case RESULTS.UNAVAILABLE:
+              console.log(
+                'This feature is not available (on this device / in this context)',
+              );
+              break;
+            case RESULTS.DENIED:
+              console.log(
+                'The permission has not been requested / is denied but requestable',
+              );
+              break;
+            case RESULTS.LIMITED:
+              console.log(
+                'The permission is limited: some actions are possible',
+              );
+              break;
+            case RESULTS.GRANTED:
+              console.log('The permission is granted');
+              break;
+            case RESULTS.BLOCKED:
+              console.log(
+                'The permission is denied and not requestable anymore',
+              );
+              break;
+          }
+        })
+        .catch(error => {
+          console.log('ERROR: ', error);
+        });
+    }
+  };
+
+  return (
+    <View>
+      <Text>Claim</Text>
+    </View>
+  );
+};
+
+export default Claim;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,10 +2,16 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {BLACK} from '../../constants/color';
 
-const Login = () => {
+const Login = ({navigation}) => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{color: BLACK}}>Login</Text>
+      <Text
+        onPress={() => {
+          navigation.navigate('Claim');
+        }}
+        style={{color: BLACK}}>
+        Login
+      </Text>
     </View>
   );
 };
